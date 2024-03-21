9031fa916d424532b83ee031c267190f73db034d
breadbread1984 <breadbread1984@163.com>
2024-03-21 16:37:35 +0800

    update

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 from typing import List
 import torch
 from huggingface_hub import login
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, LogitsProcessorList, TemperatureLogitsWarper, TopPLogitsWarper
 from langchain.llms.base import LLM
 
 class ChatGLM3(LLM):
@@ -31,3 +31,28 @@ class ChatGLM3(LLM):
   @property
   def _llm_type(self):
     return "ChatGLM3-6B"
+
+class Llama2(LLM):
+  tokenizer: AutoTokenizer = None
+  model: AutoModelforCausalLM = None
+  def __init__(self, device = 'cuda'):
+    assert device in {'cpu', 'cuda'}
+    super().__init__()
+    login(token = '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')
+    self.tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-chat-hf')
+    self.model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf')
+    self.model = self.model.to(torch.device(device))
+    self.model.eval()
+  def _call(self, prompt, stop = None, run_manager = None, **kwargs):
+    logits_processor = LogitsProcessorList()
+    logits_processor.append(TemperatureLogitsWarper(0.8))
+    logits_processor.append(TopPLogitsWarper(0.8))
+    inputs = self.tokenizer(prompt, return_tensors = 'pt', padding = True)
+    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True)
+    input_ids = outputs.sequences
+    outputs = tokenizer.batch_decode(input_ids, skip_special_tokens = True)
+    response = outputs[0][len(prompt):]
+    return response
+  @property
+  def _llm_type(self):
+    return "Llama-2-7b-chat-hf"
