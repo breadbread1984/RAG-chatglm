1159a21d1969620fc83ec86ce67a242a524578df
breadbread1984 <breadbread1984@163.com>
2024-03-21 17:08:18 +0800

    update

--- models.py
+++ models.py
@@ -48,7 +48,7 @@ class Llama2(LLM):
     logits_processor.append(TemperatureLogitsWarper(0.8))
     logits_processor.append(TopPLogitsWarper(0.8))
     inputs = self.tokenizer(prompt, return_tensors = 'pt')
-    inputs = inputs.to(torch.device(device))
+    inputs = inputs.to(torch.device(self.model.device))
     outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True)
     input_ids = outputs.sequences
     outputs = tokenizer.batch_decode(input_ids, skip_special_tokens = True)
