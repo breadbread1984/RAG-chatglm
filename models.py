05b540e573003645ffcdffd854969de9decdcca0
breadbread1984 <breadbread1984@163.com>
2024-04-18 16:46:03 +0800

    update

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 from typing import List
 import torch
 from huggingface_hub import login
-from transformers import AutoTokenizer, AutoModelForCausalLM, LogitsProcessorList, TemperatureLogitsWarper, TopPLogitsWarper
+from transformers import AutoTokenizer, AutoModelForCausalLM, LogitsProcessorList, TemperatureLogitsWarper, TopPLogitsWarper, TopKLogitsWarper, RepetitionPenaltyLogitsProcessor
 from langchain.llms.base import LLM
 
 class ChatGLM3(LLM):
@@ -61,7 +61,6 @@ class Llama2(LLM):
 class ChemDFM(LLM):
   tokenizer: AutoTokenizer = None
   model: AutoModelForCausalLM = None
-  history: List = None
   def __init__(self, device = 'cuda'):
     assert device in {'cpu', 'cuda'}
     super().__init__()
@@ -70,23 +69,19 @@ class ChemDFM(LLM):
     self.model = AutoModelForCausalLM.from_pretrained('OpenDFM/ChemDFM-13B-v1.0')
     self.model = self.model.to(torch.device(device))
     self.model.eval()
-    self.history = list()
   def _call(self, prompt, stop = None, run_manager = None, **kwargs):
     logits_processor = LogitsProcessorList()
     logits_processor.append(TemperatureLogitsWarper(0.9))
+    logits_processor.append(TopKLogitsWarper(20))
     logits_processor.append(TopPLogitsWarper(0.9))
-    s = ''
-    for idx, (q, a) in enumerate(self.history[-16:]):
-      s += '[Round %d]\nHuman: %s\nAssistant: %s\n' % (idx, q, a)
-    s += '[Round %d]\nHuman: %s\nAssistant: ' % (len(self.history[-4:]), prompt)
+    logits_processor.append(RepetitionPenaltyLogitsProcessor(1.05))
+    s = '[Round 0]\nHuman: %s\nAssistant: ' % prompt
     inputs = self.tokenizer(s, return_tensors = 'pt')
     inputs = inputs.to(torch.device(self.model.device))
-    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True, max_new_tokens = 2048)
+    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = False, use_cache = True, return_dict_in_generate = True, max_new_tokens = 2048)
     input_ids = outputs.sequences
     outputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens = True)
     response = outputs[0][len(prompt):]
-    self.history.append((prompt, response))
-    while len(self.history) > 4: self.history.pop(0)
     return response
   @property
   def _llm_type(self):
