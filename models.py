e47c280c68cd19f791e16856192e399b74657165
breadbread1984 <breadbread1984@163.com>
2024-04-25 11:26:18 +0800

    update

--- models.py
+++ models.py
@@ -65,17 +65,17 @@ class Llama3(LLM):
     assert device in {'cpu', 'cuda'}
     super().__init__()
     login(token = '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')
-    self.tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B')
-    self.model = AutoModelForCausalLM.from_pretrained('meta-llama/Meta-Llama-3-8B')
+    self.tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct')
+    self.model = AutoModelForCausalLM.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct')
     self.model = self.model.to(torch.device(device))
     self.model.eval()
   def _call(self, prompt, stop = None, run_manager = None, **kwargs):
     logits_processor = LogitsProcessorList()
-    logits_processor.append(TemperatureLogitsWarper(0.8))
-    logits_processor.append(TopPLogitsWarper(0.8))
-    inputs = self.tokenizer(prompt, return_tensors = 'pt')
+    logits_processor.append(TemperatureLogitsWarper(0.6))
+    logits_processor.append(TopPLogitsWarper(0.9))
+    inputs = self.tokenizer.apply_chat_template([{'role': 'user', 'content': prompt}], add_generation_prompt = True, return_tensors = "pt")
     inputs = inputs.to(torch.device(self.model.device))
-    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True)
+    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True, eos_token_id = [self.tokenizer.eos_token_id, self.tokenizer.convert_tokens_to_ids('<|eot_id|>')])
     input_ids = outputs.sequences
     outputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens = True)
     response = outputs[0][len(prompt):]
